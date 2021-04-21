5ecda1f5c6418443844085b26a668453a72fd804
Chirag Paladiya <chirag.paladiya@hotmail.com>
2021-04-20 22:07:18 -0400

    Update : Update file

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -10,7 +10,7 @@
 class Vehicle;
 
 
-// FP.3 Define a class „MessageQueue“ which has the public methods send and receive. 
+// FP.3 Define a class âMessageQueueâ which has the public methods send and receive. 
 // Send should take an rvalue reference of type TrafficLightPhase whereas receive should return this type. 
 // Also, the class should define an std::dequeue called _queue, which stores objects of type TrafficLightPhase. 
 // Also, there should be an std::condition_variable as well as an std::mutex as private members. 
@@ -31,11 +31,11 @@ std::condition_variable _cond;
     
 };
 
-// FP.1 : Define a class „TrafficLight“ which is a child class of TrafficObject. 
-// The class shall have the public methods „void waitForGreen()“ and „void simulate()“ 
-// as well as „TrafficLightPhase getCurrentPhase()“, where TrafficLightPhase is an enum that 
-// can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
-// Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value. 
+// FP.1 : Define a class âTrafficLightâ which is a child class of TrafficObject. 
+// The class shall have the public methods âvoid waitForGreen()â and âvoid simulate()â 
+// as well as âTrafficLightPhase getCurrentPhase()â, where TrafficLightPhase is an enum that 
+// can be either âredâ or âgreenâ. Also, add the private method âvoid cycleThroughPhases()â. 
+// Furthermore, there shall be the private member _currentPhase which can take âredâ or âgreenâ as its value. 
 
 class TrafficLight : public TrafficObject
 {
