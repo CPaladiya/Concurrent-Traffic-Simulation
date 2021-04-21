e66aec60c25ddc073a3546536ec0dcffbb79b65a
Chirag Paladiya <chirag.paladiya@hotmail.com>
2021-04-20 22:10:25 -0400

    Chore : update comments

--- src/Intersection.cpp
+++ src/Intersection.cpp
@@ -112,7 +112,7 @@ void Intersection::setIsBlocked(bool isBlocked)
 void Intersection::simulate() // using threads + promises/futures + exceptions
 {
     // FP.6a : In Intersection.h, add a private member _trafficLight of type TrafficLight. At this position, start the simulation of _trafficLight.
-    _trafficLight.simulate();
+    _trafficLight.simulate(); ///////////////// CPPND I UPDATED /////////////////////
     // launch vehicle queue processing in a thread
     threads.emplace_back(std::thread(&Intersection::processVehicleQueue, this));
 }
